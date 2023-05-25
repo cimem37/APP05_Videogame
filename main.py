f0d6b7739b2e10ea394269cdfd63c96daabc44d5
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-05-25 09:15:30 +0100

    added spawn of start screen enter sign
    player presses any button to start / mp3 background music in now included too .

--- main.py
+++ main.py
@@ -16,19 +16,59 @@ background_image = pygame.image.load("background2.jpg").convert()
 # Scale the background image to fit the screen
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 
+# Load the background music
+pygame.mixer.music.load("background_music.mp3")
+pygame.mixer.music.play(-1)  # Play the background music indefinitely (-1)
+
+# Load the "Press any key to start" image
+press_image = pygame.image.load("press_signal.png").convert_alpha()
+
+# Get the dimensions of the press image
+press_width = press_image.get_width()
+press_height = press_image.get_height()
+
+# Position the press image at the top-middle of the screen
+press_x = (screen_width - press_width) // 2
+press_y = screen_height // 4
+
+# Variables for controlling the flashing effect
+fade_alpha = 255  # Initial alpha value (fully opaque)
+fade_speed = 3    # Speed of fading (lower values mean slower fading)
+fade_direction = 1  # Direction of fading (1 = fade in, -1 = fade out)
+
 # Game loop
-running = True
-while running:
+start_game = False
+clock = pygame.time.Clock()
+
+while not start_game:
     # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            start_game = True
+
+    # Flashing effect for the press image
+    fade_alpha += fade_direction * fade_speed
+    if fade_alpha > 255:
+        fade_alpha = 255
+        fade_direction = -1
+    elif fade_alpha < 0:
+        fade_alpha = 0
+        fade_direction = 1
 
     # Draw on the screen
     screen.blit(background_image, (0, 0))  # Draw the background image at (0, 0)
+    press_image.set_alpha(fade_alpha)  # Set the alpha value for fading effect
+    screen.blit(press_image, (press_x, press_y))  # Draw the press image
 
     # Update the display
     pygame.display.flip()
+    clock.tick(60)  # Limit the frame rate to 60 FPS
+
+# Game logic and main game loop go here
+# ...
 
 # Quit Pygame
 pygame.quit()
