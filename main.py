da70340e96974a905aaa5e40753e2d534f8a9683
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-05-25 08:36:37 +0100

    Launch with background
    Background is enabled from start

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
+import sys
 
 # Initialize Pygame
 pygame.init()
 
 # Set up the display
-screen_width = 800
-screen_height = 600
+screen_width = 1280
+screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pygame Window")
+
+# Load the background image
+background_image = pygame.image.load("background2.jpg").convert()
+
+# Scale the background image to fit the screen
+background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 
 # Game loop
 running = True
@@ -17,11 +24,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Update game logic
-
     # Draw on the screen
-    screen.fill((255, 255, 255))  # Fill the screen with white
-    # Add your drawing code here
+    screen.blit(background_image, (0, 0))  # Draw the background image at (0, 0)
 
     # Update the display
     pygame.display.flip()
