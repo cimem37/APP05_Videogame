60a62e57d740069c5d2204829f42e7146641e06f
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-05-25 22:53:36 +0100

    Alien 1
    UFO Enemy shoots projectiles around

--- main.py
+++ main.py
@@ -1,71 +1,78 @@
 import pygame
 import sys
+import random
 
 # Initialize Pygame
 pygame.init()
 
-# Set up the display
+
 screen_width = 1280
 screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pygame Window")
 
-# Load the background image
+
 background_image = pygame.image.load("background2.jpg").convert()
-# Scale the background image to fit the screen
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 
-# Load the background music
-pygame.mixer.music.load("background_music.mp3")
-pygame.mixer.music.play(-1)  # Play the background music indefinitely (-1)
-
-# Load the "Press any key to start" image
-press_image = pygame.image.load("press_signal.png").convert_alpha()
-
-# Get the dimensions of the press image
-press_width = press_image.get_width()
-press_height = press_image.get_height()
-
-# Position the press image at the top-middle of the screen
-press_x = (screen_width - press_width) // 2
-press_y = screen_height // 4
-
-# Variables for controlling the flashing effect
-fade_alpha = 255  # Initial alpha value (fully opaque)
-fade_speed = 3    # Speed of fading (lower values mean slower fading)
-fade_direction = 1  # Direction of fading (1 = fade in, -1 = fade out)
-
-# Load the character image
 character_image = pygame.image.load("character.png").convert_alpha()
-
-# Resize the character image
 character_width = 100
 character_height = 100
 character_image = pygame.transform.scale(character_image, (character_width, character_height))
 
-# Position the character at the middle of the left side of the bottom screen
+ufo_image = pygame.image.load("ufo.png").convert_alpha()
+ufo_width = 100
+ufo_height = 100
+ufo_image = pygame.transform.scale(ufo_image, (ufo_width, ufo_height))
+
+projectile_image = pygame.image.load("beam.png").convert_alpha()
+projectile_width = 30
+projectile_height = 30
+projectile_image = pygame.transform.scale(projectile_image, (projectile_width, projectile_height))
+
 character_x = 0
 character_y = screen_height - character_height
 
-# Variables for controlling the character movement
+ufo_x = (screen_width - ufo_width) // 2
+ufo_y = 100
+
+projectile_x = ufo_x + ufo_width // 2 - projectile_width // 2
+projectile_y = ufo_y + ufo_height
+
 character_speed = 5
 is_jumping = False
 jump_count = 10
 
-# Game loop
+ufo_speed = 3
+is_ufo_moving_right = True
+
+projectile_speed = 6
+is_projectile_active = False
+
+pygame.mixer.music.load("background_music.mp3")
+pygame.mixer.music.play(-1)  # Play the background music indefinitely (-1)
+
+press_image = pygame.image.load("press_signal.png").convert_alpha()
+press_width = press_image.get_width()
+press_height = press_image.get_height()
+
+press_x = (screen_width - press_width) // 2
+press_y = screen_height // 4
+
+fade_alpha = 255  # Initial alpha value (fully opaque)
+fade_speed = 3    # Speed of fading (lower values mean slower fading)
+fade_direction = 1  # Direction of fading (1 = fade in, -1 = fade out)
+
 start_game = False
 clock = pygame.time.Clock()
 
 while not start_game:
-    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             start_game = True
-
-    # Flashing effect for the press image
     fade_alpha += fade_direction * fade_speed
     if fade_alpha > 255:
         fade_alpha = 255
@@ -74,18 +81,16 @@ while not start_game:
         fade_alpha = 0
         fade_direction = 1
 
-    # Draw on the screen
     screen.blit(background_image, (0, 0))  # Draw the background image at (0, 0)
     press_image.set_alpha(fade_alpha)  # Set the alpha value for fading effect
     screen.blit(press_image, (press_x, press_y))  # Draw the press image
 
     # Update the display
     pygame.display.flip()
-    clock.tick(60)  # Limit the frame rate to 60 FPS
+    clock.tick(60)
 
-# Main game loop
+
 while True:
-    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -117,20 +122,51 @@ while True:
             is_jumping = False
             jump_count = 10
 
-    # Ensure the character stays within the screen boundaries
     if character_x < 0:
         character_x = 0
     elif character_x > screen_width - character_width:
         character_x = screen_width - character_width
 
+    if is_ufo_moving_right:
+        ufo_x += ufo_speed
+        if ufo_x > screen_width - ufo_width:
+            ufo_x = screen_width - ufo_width
+            is_ufo_moving_right = False
+    else:
+        ufo_x -= ufo_speed
+        if ufo_x < 0:
+            ufo_x = 0
+            is_ufo_moving_right = True
+
+    # Projectile logic
+    if not is_projectile_active:
+        # Calculate the position of the projectile relative to the UFO
+        projectile_x = ufo_x + ufo_width // 2 - projectile_width // 2
+        projectile_y = ufo_y + ufo_height
+
+
+        is_projectile_active = True
+
+    # Move the projectile downwards
+    if is_projectile_active:
+        projectile_y += projectile_speed
+
+        # Check if the projectile has reached the character
+        if projectile_y > character_y + character_height:
+            is_projectile_active = False
+
     # Draw on the screen
     screen.blit(background_image, (0, 0))  # Draw the background image at (0, 0)
     screen.blit(character_image, (character_x, character_y))  # Draw the character
+    screen.blit(ufo_image, (ufo_x, ufo_y))  # Draw the UFO
+
+    if is_projectile_active:
+        screen.blit(projectile_image, (projectile_x, projectile_y))  # Draw the projectile
 
     # Update the display
     pygame.display.flip()
     clock.tick(60)  # Limit the frame rate to 60 FPS
 
-# Quit Pygame
+# End the game
 pygame.quit()
 sys.exit()
