06dbe06006a6b430e56000b751ae3cc82fde3635
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-05-26 05:08:25 +0100

    2 UFO / HP BAR / STACK OF
    Upgraded the level of the game  , essentially increased its difficulty by doubling the enemies & Introduced the maximum of 5 shots ( to empty the HP bar ) followed in case of failure by a GAME OVER prompt message .

--- main.py
+++ main.py
@@ -11,83 +11,68 @@ screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pygame Window")
 
-# Load the background image
+# Load the images
 background_image = pygame.image.load("background2.jpg").convert()
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 
-# Load the character image
 character_image = pygame.image.load("character.png").convert_alpha()
 character_width = 100
 character_height = 100
 character_image = pygame.transform.scale(character_image, (character_width, character_height))
 
-# Load the UFO image
 ufo_image = pygame.image.load("ufo.png").convert_alpha()
 ufo_width = 150
 ufo_height = 100
 ufo_image = pygame.transform.scale(ufo_image, (ufo_width, ufo_height))
 
-# Load the beam image
 beam_image = pygame.image.load("beam.png").convert_alpha()
 beam_width = 20
 beam_height = 50
 beam_image = pygame.transform.scale(beam_image, (beam_width, beam_height))
 
-# Load the press signal image
-press_signal_image = pygame.image.load("press_signal.png").convert_alpha()
-press_signal_width = press_signal_image.get_width()
-press_signal_height = press_signal_image.get_height()
-press_signal_alpha = 255  # Initial alpha value for flashy/fading effect
-press_signal_image = pygame.transform.scale(press_signal_image, (press_signal_width, press_signal_height))
+life_bar_width = 200
+life_bar_height = 20
 
-# Position the character at the middle of the left side of the bottom screen
+# Set initial positions
 character_x = 0
 character_y = screen_height - character_height
 
-# Position the UFO at the top-middle of the screen
 ufo_x = (screen_width - ufo_width) // 2
 ufo_y = 100
 
-# Position the beam initially off-screen
+ufo2_x = 0
+ufo2_y = 100
+
 beam_x = -beam_width
 beam_y = -beam_height
 
-# Position the press signal in the center of the screen
-press_signal_x = (screen_width - press_signal_width) // 2
-press_signal_y = (screen_height - press_signal_height) // 2
-
-# Variables for controlling the character movement
 character_speed = 5
 is_jumping = False
 jump_count = 10
 
-# Variables for controlling the UFO movement
 ufo_speed = 2
+ufo2_speed = 2
 
-# Variables for controlling the beam
 beam_speed = 5
 beam_active = False
 beam_timer = 0
 beam_interval = random.randint(2000, 4000)
 
-# Variables for registering successful beam hits
-hits = 0
+beam2_active = False
+beam2_timer = 0
+beam2_interval = random.randint(2000, 4000)
 
-# Load the background music
+score = 0
+hit_beams = 0
+life = 100
+
 pygame.mixer.music.load("background_music.mp3")
 pygame.mixer.music.play(-1)
 
-# Game loop
 start_game = False
 clock = pygame.time.Clock()
 
-show_press_signal = True
-flash_timer = 0
-flash_interval = 500  # Time interval for flashing/fading effect
-is_flashing = True  # Flag for determining whether to show or hide the press signal
-
 while not start_game:
-    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -95,31 +80,11 @@ while not start_game:
         elif event.type == pygame.KEYDOWN:
             start_game = True
 
-    # Draw on the screen
-    screen.blit(background_image, (0, 0))  # Draw the background image
-
-    if show_press_signal:
-        if is_flashing:
-            press_signal_image.set_alpha(press_signal_alpha)  # Set the alpha value for flashing/fading
-            screen.blit(press_signal_image, (press_signal_x, press_signal_y))  # Draw the press signal image
-
-        flash_timer += clock.get_time()
-        if flash_timer >= flash_interval:
-            flash_timer = 0
-            if is_flashing:
-                press_signal_alpha = 0  # Hide the press signal by setting alpha to 0
-                is_flashing = False
-            else:
-                press_signal_alpha = 255  # Show the press signal by setting alpha to 255
-                is_flashing = True
-
-    # Update the display
+    screen.blit(background_image, (0, 0))
     pygame.display.flip()
-    clock.tick(60)  # Limit the frame rate to 60 FPS
+    clock.tick(60)
 
-# Main game loop
 while True:
-    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -166,15 +131,35 @@ while True:
     elif ufo_x > screen_width - ufo_width:
         ufo_x = screen_width - ufo_width
 
+    # Update the position of the second UFO
+    if ufo2_x < screen_width - ufo_width:
+        ufo2_x += ufo2_speed
+    else:
+        ufo2_x = 0
+
     if beam_active:
         beam_y += beam_speed
         if beam_y > screen_height:
             beam_active = False
+            score += 5
 
         if character_x < beam_x + beam_width and character_x + character_width > beam_x \
                 and character_y < beam_y + beam_height and character_y + character_height > beam_y:
-            hits += 1
+            hit_beams += 1
             beam_active = False
+            life -= 20
+
+    if beam2_active:
+        beam2_y += beam_speed
+        if beam2_y > screen_height:
+            beam2_active = False
+            score += 5
+
+        if character_x < beam2_x + beam_width and character_x + character_width > beam2_x \
+                and character_y < beam2_y + beam_height and character_y + character_height > beam2_y:
+            hit_beams += 1
+            beam2_active = False
+            life -= 20
 
     if not beam_active:
         beam_timer += clock.get_time()
@@ -185,16 +170,41 @@ while True:
             beam_timer = 0
             beam_interval = random.randint(2000, 4000)
 
+    if not beam2_active:
+        beam2_timer += clock.get_time()
+        if beam2_timer >= beam2_interval:
+            beam2_x = ufo2_x + ufo_width // 2 - beam_width // 2
+            beam2_y = ufo2_y + ufo_height
+            beam2_active = True
+            beam2_timer = 0
+            beam2_interval = random.randint(2000, 4000)
+
     screen.blit(background_image, (0, 0))
     screen.blit(character_image, (character_x, character_y))
     screen.blit(ufo_image, (ufo_x, ufo_y))
+    screen.blit(ufo_image, (ufo2_x, ufo2_y))
     if beam_active:
         screen.blit(beam_image, (beam_x, beam_y))
+    if beam2_active:
+        screen.blit(beam_image, (beam2_x, beam2_y))
+
+    # Draw the life bar
+    pygame.draw.rect(screen, (255, 0, 0), (screen_width - life_bar_width - 20, 20, life_bar_width, life_bar_height))
+    life_bar_fill = max(0, life / 100) * life_bar_width
+    pygame.draw.rect(screen, (0, 255, 0), (screen_width - life_bar_width - 20, 20, life_bar_fill, life_bar_height))
 
     font = pygame.font.Font(None, 36)
-    text = font.render("Hits: " + str(hits), True, (255, 255, 255))
-    screen.blit(text, (10, 10))
+    score_text = font.render("Score: " + str(score), True, (255, 255, 255))
+    hit_text = font.render("Hits: " + str(hit_beams), True, (255, 255, 255))
+    screen.blit(score_text, (screen_width // 2 - score_text.get_width() // 2, 10))
+    screen.blit(hit_text, (10, 10))
 
-    # Update the display
+    if life <= 0:
+        game_over_text = font.render("Game Over", True, (255, 0, 0))
+        screen.blit(game_over_text, (screen_width // 2 - game_over_text.get_width() // 2, screen_height // 2))
+
     pygame.display.flip()
-    clock.tick(60)  # Limit the frame rate to 60 FPS
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
